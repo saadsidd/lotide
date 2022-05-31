2ac048b5f1f6bb85642482ea529dae0027903aa5
Saad Siddiq <siddiq.saad@gmail.com>
2022-05-30 19:00:21 -0500

    function to retrieve array tail

---
+++ tail.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  return array.slice(1, array.length);
+}
+
+const words = ['Yo Yo', 'Lighthouse', 'Labs'];
+tail(words);
+assertEqual(words.length, 3);
