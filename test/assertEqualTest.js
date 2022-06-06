4f6a9e008c06d39605ce63e7eddd8eb491ac3ea4
Saad Siddiq <siddiq.saad@gmail.com>
2022-06-06 05:01:52 -0500

    separated test cases

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+
+// Test code for assertEqual.js
+assertEqual('Test', 'Test');
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(2, 3);
