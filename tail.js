8549ced3777db2187b8aa3a866d8d3cacf8a96a3
Saad Siddiq <siddiq.saad@gmail.com>
2022-06-06 05:08:58 -0500

    refactor to export as module

--- tail.js
+++ tail.js
@@ -1,16 +1,6 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+// Returns array from 2nd element onward
 const tail = function(array) {
   return array.slice(1, array.length);
 };
 
-const words = ['Yo Yo', 'Lighthouse', 'Labs'];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
