d82e558d8c0c5964956f6dc7413c921bfbe5ba42
Saad Siddiq <siddiq.saad@gmail.com>
2022-06-06 05:27:40 -0500

    refactor to export as module

--- middle.js
+++ middle.js
@@ -1,26 +1,4 @@
-// Determines whether two given arrays are equal
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// Calls eqArrays and asserts whether two given arrays are equal
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: The arrays are equal`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: The arrays are NOT equal`);
-  }
-};
-
-// Function to return middle element of array (or two elements if length is odd)
+// Return middle element of array (or two elements as array if length is odd)
 const middle = function(array) {
   if (array.length <= 2) {
     return [];
@@ -31,13 +9,4 @@ const middle = function(array) {
   return [array[Math.floor(array.length / 2)]];
 };
 
-
-// Test cases
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+module.exports = middle;
