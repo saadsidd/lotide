73bea0e506d26351f39eb265b74c9cdb9254136e
Saad Siddiq <siddiq.saad@gmail.com>
2022-06-06 13:38:47 -0500

    refactor to use mocha/chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,41 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
 // Test cases for middle.js
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+describe('#middle', () => {
+  it('should return [] when given [1]', () => {
+    const input = [1];
+    const output = [];
+    assert.deepEqual(middle(input), output);
+  });
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+  it('should return [] when given [1, 2]', () => {
+    const input = [1, 2];
+    const output = [];
+    assert.deepEqual(middle(input), output);
+  });
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  it('should return [2] when given [1, 2, 3]', () => {
+    const input = [1, 2, 3];
+    const output = [2];
+    assert.deepEqual(middle(input), output);
+  });
+
+  it('should return [3] when given [1, 2, 3, 4, 5]', () => {
+    const input = [1, 2, 3];
+    const output = [2];
+    assert.deepEqual(middle(input), output);
+  });
+
+  it('should return [2, 3] when given [1, 2, 3, 4]', () => {
+    const input = [1, 2, 3];
+    const output = [2];
+    assert.deepEqual(middle(input), output);
+  });
+
+  it('should return [3, 4] when given [1, 2, 3, 4, 5, 6]', () => {
+    const input = [1, 2, 3];
+    const output = [2];
+    assert.deepEqual(middle(input), output);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,17 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// Test code for tail.js
-assertEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']).length, 2);
-assertEqual(tail([1, 2, 3, 4, 5, 6, 7]).length, 6);
+// Test cases for tail.js
+describe('#tail', () => {
+  it('should return [2, 3, 4, 5, 6, 7] when given [1, 2, 3, 4, 5, 6, 7]', () => {
+    const input = [1, 2, 3, 4, 5, 6, 7];
+    const output = [2, 3, 4, 5, 6, 7];
+    assert.deepEqual(tail(input), output);
+  });
+
+  it('should return ["Lighthouse", "Labs"] when given ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    const input = ['Yo Yo', 'Lighthouse', 'Labs'];
+    const output = ['Lighthouse', 'Labs'];
+    assert.deepEqual(tail(input), output);
+  });
+});
