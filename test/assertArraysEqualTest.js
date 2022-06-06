9cba4dc6b4bf2ed3907725a19d144087223b675b
Saad Siddiq <siddiq.saad@gmail.com>
2022-06-06 05:19:44 -0500

    separated test cases

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,7 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+// Test cases for assertArraysEqual.js
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
